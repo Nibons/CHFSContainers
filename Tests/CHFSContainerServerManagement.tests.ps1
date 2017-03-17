9f4906257269de44b9c2af03284a26e16457c686
nibons <imac5640@gmail.com>
2017-03-17 10:59:12 -0400

    added passing pester test

--- Tests/CHFSContainerServerManagement.tests.ps1
+++ Tests/CHFSContainerServerManagement.tests.ps1
@@ -3,3 +3,9 @@ $repositoryRoot = Split-Path -parent -Path $here
 
 $module_Root = "$repositoryRoot\CHFSContainerServerManagement"
 import-module $module_Root -Force
+
+Describe 'invokes pester tests' {
+    Context 'running in pester' {
+        $true | should be $true
+    }
+}
